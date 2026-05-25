fd69d92e3980fb08d9fe4ffd6a7e3e9194573704
Rion0-0 <rino.iwsk@gmail.com>
2026-05-25 16:20:41 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,63 @@
+export const MEMBERS = [
+  "Kis-My-Ft2",
+  "千賀健永",
+  "宮田俊哉",
+  "横尾渉",
+  "藤ヶ谷太輔",
+  "玉森裕太",
+  "二階堂高嗣"
+];
+
+export const KINDS = [
+  "仕事",
+  "事務"
+];
+
+export const CATEGORIES = [
+  "📺 TV",
+  "📻 RADIO",
+  "📚 雑誌",
+  "💿 CD/DVD",
+  "🎬 ドラマ/映画",
+  "🌐 WEB",
+  "🎤 ライブ/イベント",
+  "🎫 申込開始",
+  "⏰ 申込締切",
+  "✅ 当落発表",
+  "💸 入金期限",
+  "🛍 グッズ",
+  "🚄 遠征",
+  "🏨 ホテル",
+  "📦 発送/受取",
+  "📝 メモ"
+];
+
+export const PRIORITIES = [
+  {
+    label:"🔥重要",
+    value:"high"
+  },
+  {
+    label:"普通",
+    value:"normal"
+  },
+  {
+    label:"ゆるめ",
+    value:"low"
+  }
+];
+
+export const REPEATS = [
+  {
+    label:"繰り返しなし",
+    value:"none"
+  },
+  {
+    label:"毎週",
+    value:"weekly"
+  },
+  {
+    label:"毎月",
+    value:"monthly"
+  }
+];
